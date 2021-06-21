aa27cc6322e2a7e3d7d67eca1f2b294af3e80513
Charles Tenorio <charlestenorios@gmail.com>
2021-06-20 21:28:38 -0300

    domani ok

--- Prometheus.Domain/Entities/Prefeitura.cs
+++ Prometheus.Domain/Entities/Prefeitura.cs
@@ -1,35 +1,43 @@
 using System;
-
+using System.Collections.Generic;
 
 namespace Prometheus.Domain.Entities
 {
-    public sealed class Prefeitura
+    public sealed class Prefeitura : Base
     {
-        public Prefeitura(string cnpj, string nome, string email, string fone)
+        public Prefeitura(int id, string cnpj, string nome, string email, string fone, string imagem)
         {
-            this.cnpj = cnpj;
-            this.nome = nome;
-            this.email = email;
-            this.fone = fone;
+            this.Id = id;
+            ValidateDomain(cnpj, nome, email, fone, imagem);
+            this.DataAt= DateTime.Now;
         }
 
-        public string cnpj{get; private set;}
-        public string nome{get; private set;}
-        public string email{get; private set;}
 
-        public string fone{get; private set;}
+         public void Update(int id, string cnpj, string nome, string email, string fone, string imagem)
+        {
+            this.Id = id;
+            ValidateDomain(cnpj, nome, email, fone, imagem);
+            this.DataAt= DateTime.Now;
+        }
 
-        private void ValidateDomain(string cnpj, string nome, string email, string fone){
+       
+        public string Image{get; private set;}
+
+        public ICollection<Secretaria> Secretarias{get; set;}
+
+        private void ValidateDomain(string cnpj, string nome, string email, string fone, string imagem){
             DomainExceptionValidation.When(string.IsNullOrEmpty(nome), "Nome nao pode ser null ou em branco");
-            this.nome = nome;
+            DomainExceptionValidation.When(DomainExceptionValidation.IsCnpj(cnpj), "CNPJ Invalido");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(email), "Email nao pode ser nulo");
+            DomainExceptionValidation.When(DomainExceptionValidation.IsValidEmail(email), "Formato de email invalido");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(fone), "Fone nao pode ser Nulo ou branco");
+            DomainExceptionValidation.When(Image.Length>250, "nome da imagem so pode ter 256");
             
-            if(DomainExceptionValidation.IsCnpj(cnpj)){
-               this.cnpj=cnpj;
-
-            }
-            else{
-                throw
-            }
+            this.Nome = nome;
+            this.Cnpj = cnpj;
+            this.Fone=fone;
+            this.Email =email;
+            this.Image =imagem;
 
 
         }
---
+++ Prometheus.Domain/Entities/Secretaria.cs
@@ -0,0 +1,50 @@
+using System;
+
+
+namespace Prometheus.Domain.Entities
+{
+    public sealed class Secretaria : Base
+    {
+        
+        public int CodigoPrefeitura { get; set ; }
+       
+        public Prefeitura prefeitura {get; set;}
+
+        public Secretaria(string cnpj, int id, int codigoPrefeitura, string nome, string fone, string email, Prefeitura prefeitura)
+        {
+            this.Id = id;
+            this.CodigoPrefeitura = codigoPrefeitura;
+            ValidateDomain(cnpj, nome, fone, email);
+            this.prefeitura = prefeitura;
+            this.DataAt=DateTime.Now;
+        }
+
+
+        public void Update(string cnpj, int id, int codigoPrefeitura, string nome, string fone, string email, Prefeitura prefeitura)
+        {
+            this.Id = id;
+            this.CodigoPrefeitura = codigoPrefeitura;
+            ValidateDomain(cnpj, nome, fone, email);
+            this.prefeitura = prefeitura;
+        }
+
+
+         private void ValidateDomain(string cnpj, string nome, string fone, string email){
+             DomainExceptionValidation.When(DomainExceptionValidation.IsCnpj(cnpj), "CNPJ Invalido");
+             this.Cnpj = cnpj;
+            
+            DomainExceptionValidation.When(string.IsNullOrEmpty(nome), "Nome nao pode ser null ou em branco");
+            this.Nome = nome;
+            
+            DomainExceptionValidation.When(string.IsNullOrEmpty(email), "Email nao pode ser nulo");
+            DomainExceptionValidation.When(DomainExceptionValidation.IsValidEmail(email), "Formato de email invalido");
+            this.Email =email;
+            
+            DomainExceptionValidation.When(string.IsNullOrEmpty(fone), "Fone nao pode ser Nulo ou branco");
+            this.Fone= fone;
+        }
+
+    }
+
+}
+ 
