d00ad039b368650e716ad2fe26c32b6cc9ee2d0c
Charles Tenorio <charlestenorios@gmail.com>
2021-07-04 23:18:38 -0300

    DbContext

--- Prometheus.Infra.Data/Context/ApplicationDbContext.cs
+++ Prometheus.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 
-
+using Prometheus.Domain.Entities;
 namespace Prometheus.Infra.Data.Context
 {
     public class ApplicationDbContext : DbContext
@@ -10,6 +10,31 @@ namespace Prometheus.Infra.Data.Context
             
         }
 
-        public DbSet<
+        public DbSet<Endereco> Enderecos {get; set;}
+        public DbSet<Prefeitura> Prefeituras {get; set;}
+        public DbSet<Secretaria> Secretarias {get; set;}
+
+        public DbSet<Funcao> Funcaos {get; set;}
+
+        public DbSet<Colaborador> Colaboradores {get; set;}
+
+        public DbSet<Cidadao> Cidadaos {get; set;}
+
+        public DbSet<Demanda> Demandas {get; set;}
+        public DbSet<Equipe> Equipes {get; set;}
+        public DbSet<Atendimento> Atendimentos {get; set;}
+
+        public DbSet<DetalheAtendimento> DetalhesAtendimentos {get; set;}
+
+        public DbSet<Chamado> Chamados {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        }
+
+
     }
 }
